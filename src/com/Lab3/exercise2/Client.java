7f086e99b64653a0b853a32daf1e8468ca2c625b
anagchaio <anagchaio@gmail.com>
2021-03-27 20:44:04 -0300

    New commit

--- src/com/Lab3/exercise2/Client.java
+++ src/com/Lab3/exercise2/Client.java
@@ -5,7 +5,6 @@ public class Client {
     private String name;
     private String email;
     private Double discount;
-    private String hoy;
 
     public Client(String name, String email, Double discount) {
         this.id = UUID.randomUUID();
